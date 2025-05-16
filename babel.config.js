9fa4c34377bfeb5275e427feed863fc7d82b5e34
Malygod <matiasscontact@gmail.com>
2025-05-16 07:50:24 -0400

    test build

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,13 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      '@babel/plugin-transform-modules-commonjs',
-      'react-native-reanimated/plugin'
-    ]
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            ['@babel/plugin-transform-modules-commonjs', {
+                allowTopLevelThis: true,
+                loose: true
+            }],
+            'react-native-reanimated/plugin'
+        ]
+    };
 };
